460a71577d915c61c4112c5ee71846b63bd41687
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-11 11:55:41 +0000

    feat: Implement insertion sort for doubly linked list and add time complexity documentation

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+/**
+ * swap_nodes - Swaps two adjacent nodes in a doubly linked list
+ *
+ * @list: Double pointer to the head of the list
+ * @node1: Pointer to the first node
+ * @node2: Pointer to the second node
+ *
+ * Return: void
+ **/
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+	node1->prev = node2;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       in ascending order using the Insertion sort algorithm
+ *
+ * @list: Double pointer to the head of the list
+ *
+ * Return: void
+ **/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	listint_t *prev;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		prev = current->prev;
+
+		while (prev && current->n < prev->n)
+		{
+			swap_nodes(list, prev, current);
+			print_list(*list);
+			prev = current->prev;
+		}
+
+		current = current->next;
+	}
+}
