5db2f3e09e23626698909f2cc4fba3d4ed63004a
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-11 10:33:58 +0000

    fix: Update documentation for bubble_sort function parameters

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,10 @@
 #include "sort.h"
 /**
- *
  * bubble_sort - Sorts an array of integers in ascending order
  *                 using the Bubble sort algorithm
  *
- * @array: The array to be sorted
- * @size: The size of the array
+ * @array: Pointer to the array to be sorted
+ * @size: Number of elements in the array
  *
  */
 void bubble_sort(int *array, size_t size)
