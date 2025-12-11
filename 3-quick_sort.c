efee98b721b90f72230bf4da1a8ffbbed26fb328
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-11 13:42:12 +0000

    feat: Implement Lomuto partition scheme and recursive quicksort function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,43 +1,73 @@
 #include "sort.h"
+
 /**
- * quick_sort - Sorts an array of integers in ascending order
+ * lomuto_partition - Partitions sub-array around pivot using Lomuto scheme
+ * @array: The array to sort
+ * @low: Start index of sub-array
+ * @high: End index of sub-array
+ * @size: Total size of the array (for printing)
  *
- * @array: Pointer to the array of integers to be sorted.
- * @size: The number of elements in the array.
- *
- * Return: void
+ * Return: The pivot index after partition
  */
-void quick_sort(int *array, size_t size)
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	size_t i, j;
-	int pivot, temp;
+	int pivot = array[high];
+	int i = low, j, temp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	pivot = array[size / 2];
-	i = 0;
-	j = size - 1;
-
-	while (i <= j)
+	for (j = low; j < high; j++)
 	{
-		while (array[i] < pivot)
-			i++;
-		while (array[j] > pivot)
-			j--;
-		if (i <= j)
+		if (array[j] <= pivot)
 		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, size);
+			if (i != j)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
 			i++;
-			if (j > 0)
-				j--;
 		}
 	}
 
-	if (j > 0)
-		quick_sort(array, j + 1);
-	quick_sort(array + i, size - i);
+	if (i != high)
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	return (i);
+}
+
+/**
+ * quicksort_rec - Recursive part of quick sort
+ * @array: array of integers
+ * @low: lowest index
+ * @high: highest index
+ * @size: array size
+ */
+void quicksort_rec(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+		quicksort_rec(array, low, pivot_index - 1, size);
+		quicksort_rec(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ * @array: Pointer to array
+ * @size: Size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quicksort_rec(array, 0, size - 1, size);
 }
