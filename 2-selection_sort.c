c9cd034376e4721b6e94d1043f22b5e3c097743d
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-11 13:21:29 +0000

    feat: Implement selection sort algorithm and add time complexity documentation

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm.
+ *
+ * @array: Pointer to the array of integers to be sorted.
+ * @size: The number of elements in the array.
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		int min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+		}
+
+		print_array(array, size);
+	}
+}
