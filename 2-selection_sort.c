bc35b5112358e87f70bb36fb1e9e9cabe06fd269
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-11 13:25:41 +0000

    fix: Refactor selection_sort for clarity and update subproject commit status to indicate dirty state

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,32 +10,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i;
-	size_t j;
+	size_t i, j, min_idx;
 	int temp;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		size_t min = i;
-
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min])
-			{
-				min = j;
-			}
+			if (array[j] < array[min_idx])
+				min_idx = j;
 		}
 
-		if (min != i)
+		if (min_idx != i)
 		{
 			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
 		}
-
-		print_array(array, size);
 	}
 }
