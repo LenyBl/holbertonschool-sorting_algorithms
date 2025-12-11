43a63536243bc6510438c8f227fa2f8b20ee6387
Leny Blee <127829557+LenyBl@users.noreply.github.com>
2025-12-11 10:06:17 +0000

    feat : Add bubble sort implementation and build configuration

---
+++ 0-bubble_sort.c, 0-O.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ *
+ * bubble_sort - Sorts an array of integers in ascending order
+ *                 using the Bubble sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i,j = 0;
+	int temp;
+
+	for (i; i < size - 1; i++)
+	{
+		for ( j; j < size - i - 1 ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}	
+		}
+	}
+}
